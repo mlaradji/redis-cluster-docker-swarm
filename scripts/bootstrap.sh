384cd540e1c8967ba35dfc1c5ce7763e71c6e4f2
Mohamed Laradji <malaradji@gmail.com>
2020-04-23 09:16:01 -0700

    chore: remove references to Thomas Fan's repo

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,38 +10,38 @@ REDIS_SENTINEL_NAME="redis-sentinel"
 REDIS_MASTER_NAME="redismaster"
 
 echo "Starting redis-zero"
-docker service create --network redis --name redis-zero redis:4.0.11-alpine
+docker service create --network webnet --name redis-zero redis:5.0.9-alpine
 
 echo "Starting services"
 docker stack deploy -c scripts/docker-compose.yml cache
 
-until [ "$(docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
+until [ "$(docker run --rm --network webnet mlaradji/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME \
 	value num-other-sentinels)" = "$((NUM_OF_SENTINELS - 1))" ]; do
 	echo "Sentinels not set up yet - sleeping"
 	sleep 2
 done
 
-until [ "$(docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
+until [ "$(docker run --rm --network webnet mlaradji/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME \
 	value "num-slaves")" = "$NUM_OF_REDIS" ]; do
 	echo "Slaves not set up yet - sleeping"
 	sleep 2
 done
 
-old_master=$(docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
+old_master=$(docker run --rm --network webnet mlaradji/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME value ip)
 echo "redis-zero ip is ${old_master}"
 
 echo "Removing redis-zero"
 docker service rm redis-zero
 
-until [ "$(docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
+until [ "$(docker run --rm --network webnet mlaradji/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME value ip)" != "$old_master" ]; do
 	echo "Failover did not happen yet - sleeping"
 	sleep 2
 done
 
 echo "Make sure the number of slaves are set"
-docker run --rm --network redis thomasjpfan/redis-utils:$TAG \
+docker run --rm --network webnet mlaradji/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME reset "num-slaves" "$((NUM_OF_REDIS - 1))"
