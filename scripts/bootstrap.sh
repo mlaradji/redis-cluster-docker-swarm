95ecf0a820b6ac8f391b49c5ac89de5ad23a6897
Mohamed Laradji <malaradji@gmail.com>
2020-04-23 11:27:37 -0700

    fix(bootstrap): combine bootstrap again

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -30,7 +30,7 @@ echo "Starting redis-zero"
 docker service create --network $NETWORK --name redis-zero redis:5.0.9-alpine
 
 echo "Starting services"
-docker stack deploy -c scripts/docker-compose.yml cache
+# docker stack deploy -c scripts/docker-compose.yml cache
 
 until [ "$(docker run --rm --network $NETWORK mlaradji/redis-utils:$TAG \
 	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME \
