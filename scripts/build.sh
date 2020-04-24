57747b06f138efc9f689ad968adb8b7792f75bd6
Mohamed Laradji <malaradji@gmail.com>
2020-04-23 19:33:36 -0700

    chore(version): add VERSION

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-TAG=${1:-"latest"}
-
+# Grab the root directory path for redis-cluster-docker-swarm.
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+# Grab the version number from version.txt.
+TAG=${1:-`cat ${ROOT}/VERSION`}
 
 for image in "redis-look" "redis-sentinel" "redis-utils"; do
 	echo "Building $image"
 	docker build -t mlaradji/${image}:${TAG} "${ROOT}/${image}"
+	docker run mlaradji/${image}:${TAG}
 	docker push mlaradji/${image}:${TAG} &
 done
 
