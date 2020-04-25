952130cdb1ba94f82df452d0c97367f548447b34
Mohamed Laradji <malaradji@gmail.com>
2020-04-24 19:20:12 -0700

    fix(deploy): test_deploy now passes successfully

--- test/test_deploy.sh
+++ test/test_deploy.sh
@@ -7,6 +7,6 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 # Grab the version number from version.txt.
 export TAG=`cat ${ROOT}/VERSION`
 
-docker network create --attachable --driver overlay redis --subnet 172.18.0.0/16
-docker stack deploy -c "$ROOT/scripts/docker-compose.yml" cache
-"$ROOT"/scripts/./bootstrap.sh
+docker network create --attachable --driver overlay redis
+docker stack deploy -c "${ROOT}/test/docker-compose.yml" cache
+"${ROOT}"/scripts/./bootstrap.sh -o3 -r3
