15a7878e28eb467e55f48b6ff051ca66bc3c2790
Mohamed Laradji <mlaradji@protonmail.ch>
2020-09-24 22:32:35 -0700

    fix(deploy): add missing REDIS_IP

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Grab the root directory path for redis-cluster-docker-swarm.
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
 # Grab the version number from version.txt.
-export TAG=`cat ${ROOT}/VERSION`
+export TAG=$(cat ${ROOT}/VERSION)
 
 NUM_OF_SENTINELS=3
 NUM_OF_REDIS=3
@@ -16,29 +16,37 @@ NETWORK="redis"
 
 # From https://unix.stackexchange.com/a/129401/304268
 while getopts ":t:n:s:o:r:m:z:" opt; do
-  case $opt in
-	t) TAG="$OPTARG"
-	;;
-    n) NETWORK="$OPTARG"
-    ;;
-	s) REDIS_SENTINEL_NAME="$OPTARG"
-	;;
-	m) REDIS_MASTER_NAME="$OPTARG"
-	;;
-	o) NUM_OF_SENTINELS="$OPTARG"
-	;;
-	r) NUM_OF_REDIS="$OPTARG"
-	;;
-	z) REDIS_ZERO_NAME="$OPTARG"
-	;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
+	case $opt in
+	t)
+		TAG="$OPTARG"
+		;;
+	n)
+		NETWORK="$OPTARG"
+		;;
+	s)
+		REDIS_SENTINEL_NAME="$OPTARG"
+		;;
+	m)
+		REDIS_MASTER_NAME="$OPTARG"
+		;;
+	o)
+		NUM_OF_SENTINELS="$OPTARG"
+		;;
+	r)
+		NUM_OF_REDIS="$OPTARG"
+		;;
+	z)
+		REDIS_ZERO_NAME="$OPTARG"
+		;;
+	\?)
+		echo "Invalid option -$OPTARG" >&2
+		;;
+	esac
 done
 
 echo $NUM_OF_SENTINELS:$TAG
 echo "Starting redis-zero"
-docker service create --network $NETWORK --name $REDIS_ZERO_NAME redis:5.0.9-alpine || true
+docker service create --network $NETWORK --name $REDIS_ZERO_NAME redis:6.0.8-alpine || true
 
 echo "Starting services"
 
